f6e7588d699753a4951f1a49879a16d40c8ef364
sfzhou567hp <260174969@qq.com>
2017-09-11 20:05:06 +0800

    test1

--- tensorflow_nmt/word2vec.py
+++ tensorflow_nmt/word2vec.py
@@ -32,7 +32,7 @@
 # print dictionary.token2id
 # new_dictionary = corpora.Dictionary.load('mydict.dic')
 # print(new_dictionary)
-
+#
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 EXAMPLE_TEXT = "Hello Mr. Smith, how are you doing today? The weather is great, and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."
